36a622f20c95df516e0a8e9a600146714b86d102
zxq <674079013@qq.com>
2017-06-04 21:12:02 +0800

    完成  :  设置界面-2 TODO:查询界面及逻辑 TODO:设置界面，关于界面及逻辑 TODO:测试，集成bugly等

--- app/src/main/java/com/kyle/mycar/Fragment/SettingOilTypeFrag.java
+++ app/src/main/java/com/kyle/mycar/Fragment/SettingOilTypeFrag.java
@@ -2,12 +2,15 @@ package com.kyle.mycar.Fragment;
 
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -15,11 +18,14 @@ import android.widget.Toast;
 import com.amulyakhare.textdrawable.TextDrawable;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.chad.library.adapter.base.entity.MultiItemEntity;
 import com.kyle.mycar.Bean.MsgChart;
 import com.kyle.mycar.R;
 import com.kyle.mycar.View.MyItemDecoration;
 import com.kyle.mycar.db.Dao.OilTypeDao;
 import com.kyle.mycar.db.Table.OilType;
+
+import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 import java.util.List;
@@ -29,12 +35,15 @@ import butterknife.Unbinder;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingOilTypeFrag extends BaseFragment implements Toolbar.OnMenuItemClickListener,  BaseQuickAdapter.OnItemChildClickListener {
+public class SettingOilTypeFrag extends BaseFragment implements Toolbar.OnMenuItemClickListener,
+        BaseQuickAdapter.OnItemChildClickListener {
 
 
     @BindView(R.id.recycler_view)
     RecyclerView recyclerView;
     Unbinder unbinder;
+    private List<OilType> mList;
+    private QuickAdapter mAdapter;
 
     @Override
     public View initView() {
@@ -43,10 +52,10 @@ public class SettingOilTypeFrag extends BaseFragment implements Toolbar.OnMenuIt
 
     @Override
     public void initData() {
-        initToolbar(R.string.oil_type, 2, R.menu.toolbar_setting_oiltype, this);
+        initToolbar(R.string.oil_type, 2, R.menu.toolbar_add, this);
         OilTypeDao typeDao = OilTypeDao.getInstance(mActivity);
-        List<OilType> list = typeDao.queryAllButIsDelete("id", true);
-        QuickAdapter mAdapter = new QuickAdapter(mActivity,list);
+        mList = typeDao.queryAllButIsDelete("id", true);
+        mAdapter = new QuickAdapter(mActivity, mList);
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager layoutManager = new LinearLayoutManager(mActivity, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
@@ -64,21 +73,37 @@ public class SettingOilTypeFrag extends BaseFragment implements Toolbar.OnMenuIt
 
     @Override
     public boolean onMenuItemClick(MenuItem item) {
+
         return false;
     }
 
     @Override
-    public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+    public void onItemChildClick(BaseQuickAdapter adapter, View view, final int position) {
+        Log.i("---", "onItemChildClick: "+position);
         switch (view.getId()) {
             case R.id.iv_delete_setting_oiltype:
-                Toast.makeText(mActivity, "ssss", Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder builder =new AlertDialog.Builder(mActivity);
+                builder.setMessage(R.string.setting_oiltype_dialog_msg)
+                        .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                OilType oilType = mList.get(position);
+                                oilType.setDelete(true);
+                                OilTypeDao.getInstance(mActivity.getApplicationContext()).update(oilType);
+                                mAdapter.getData().remove(position);
+                                mAdapter.notifyItemRemoved(position);
+                                Log.i("---", "onClick:mAdapter "+mAdapter.getData().size());
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel,null).show();
                 break;
+
             case R.id.iv_update_setting_oiltype:
+                // TODO: 2017/6/4 修改界面（使用添加界面）
                 Toast.makeText(mActivity, "sss222s", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
-
 
     static class QuickAdapter extends BaseQuickAdapter<OilType, BaseViewHolder> {
         private int[] colors;
